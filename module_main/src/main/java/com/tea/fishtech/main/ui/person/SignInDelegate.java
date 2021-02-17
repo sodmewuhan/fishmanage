65c7ca5f19cc82c4cb31066886e95e42f2f21719
sodmewuhan <1479qwert@>
2021-02-17 17:34:56 +0800

    dd

---
+++ module_main/src/main/java/com/tea/fishtech/main/ui/person/SignInDelegate.java
@@ -0,0 +1,27 @@
+package com.tea.fishtech.main.ui.person;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.tea.fishtech.ui.delegates.LatteDelegate;
+
+public class SignInDelegate extends LatteDelegate {
+
+    @Override
+    public Object setLayout() {
+        return null;
+    }
+
+    @Override
+    public void onBindView(@Nullable Bundle savedInstanceState, @NonNull View rootView) {
+
+    }
+
+    @Override
+    public void post(Runnable runnable) {
+
+    }
+}
